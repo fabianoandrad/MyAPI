7fd40eef035bd02093e8f40395da861511dcac80
fabiano_andrade <fabiano.andrade@zurg.com.br>
2021-10-08 14:27:46 -0300

    tree test

--- server.js
+++ server.js
@@ -35,13 +35,13 @@ app.post("/save", async (req, res) => {
 const data = 
   {
     "status": "false",
-    "installKey": "111cd56d-44e7-4d7f-9756-deebe848d7e7",
+    "installKey": "223cd56d-44e7-4d7f-9756-deebe848d7e7",
     "createdAt": "2021-10-06T16:43:43.3485139+00:00",
-    "enterpriseName": "teste",
+    "enterpriseName": "Jambalayaa",
     "licenseKey": "WkeSKCzSaPCwJ6ZGMnzwPw==",
-    "maxNumberOfDevices":69,
+    "maxNumberOfDevices":0,
     "licensedUntil": "2121-10-06T18:59:00.975269+00:00",
-    "licenseModel":15,
+    "licenseModel":5,
     "updatedAt": "0001-01-01T00:00:00"
 }
 
